4d01d17ef204d5315efa2fc919fee78738bb80da
Sarthak9996 <40289413+Sarthak9996@users.noreply.github.com>
2020-10-17 09:06:59 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+rev=0
+n=int(input("Enter a number to be checked : "))
+num=n
+while num!=0:
+    rem=num%10
+
+    num=num//10
+pal=rev
+if(pal==n):
+    print("The given number is a palindrome number")
+else:
+    print("The given number is not a palindrome number")
+
+print("Press enter to exit")
+
+    
